294e958e719bb180d35ffdba01298d4ef949b17b
Matt Ericksen <ericksenm93@ymail.com>
2020-07-27 21:38:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -14,8 +14,8 @@ def nyc_pigeon_organizer(data)
         if !pigeon_list[name]
           pigeon_list[name] = Hash.new
         end
-        if pigeon_list[name]
-          pigeon_list[name][k] << [k2]
+        if !pigeon_list[name][k]
+          pigeon_list[name][k] = []
         end
         puts "list: #{pigeon_list}"
       end
