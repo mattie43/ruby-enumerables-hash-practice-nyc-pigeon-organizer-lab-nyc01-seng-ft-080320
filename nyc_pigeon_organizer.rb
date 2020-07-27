8e26912c4f3bcdc5e667e83fa5c8a5faccd5efbb
Matt Ericksen <ericksenm93@ymail.com>
2020-07-27 21:41:51 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = Hash.new
+  data.each do |k,v|
+    v.each do |k2,v2|
+      v2.each do |name|
+        if !pigeon_list[name]
+          pigeon_list[name] = Hash.new
+        end
+        if !pigeon_list[name][k]
+          pigeon_list[name][k] = []
+        end
+        if pigeon_list[name][k]
+          pigeon_list[name][k] << k2.to_s
+        end
+      end
+    end
+  end
+  pigeon_list
 end
